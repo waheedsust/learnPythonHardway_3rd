892c7a978fe0f6c0d5a39f877d52575fb4ea17af
waheedsust <44333294+waheedsust@users.noreply.github.com>
2020-05-30 20:36:57 +0600

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,28 @@
+#this is will print the one string
+print("I will now conunt my chickens:")
+#this will print string within the quotation and append the arrithmatic calclation.
+print("Hens", 25 + 30 /6)
+# same as line 3
+print("Roasters", 100 - 25.3 * 3 % 4)
+#print one string
+print("Now I will count the eggs:")
+#print the output of one arrithmatic calculation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+#print one string.
+print("Is it true that 3 + 2 < 5 - 7?")
+#print the ouput of the boolean of logical output
+print(3 + 2 < 5 - 7)
+#same as line 3
+print("What is 3 + 2?", 3 + 2)
+#same as line 3
+print("What is 5 - 7?", 5 - 7)
+#same as line 11
+print("Oh, that's why it's false")
+#same as line 11
+print("How about some more.")
+#this will print string within the quotation and append the boolean calclation.
+print("Is it greater?", 5 > -2)
+#same as 24
+print("Is it greater or eqaual?", 5 >= 2)
+#same as 24
+print("Is it less or equal?", 5 <= 2)

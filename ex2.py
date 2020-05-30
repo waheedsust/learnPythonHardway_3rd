77f88f2bcdc8562bb8c9c81bed4d68c8a1b3c4d5
waheedsust <44333294+waheedsust@users.noreply.github.com>
2020-05-30 17:19:34 +0600

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,6 @@
+#exercise 2
+# A comment, this so you can read the program later
+# Anything after # is ignored by Python
+print("I could have code like this") # and the comment after
+# you can also use # do "disable" or comment out a code
+#print("This won't run")

62f43a7fcb1d31e992bd1c098bf8f27c9d8c340b
waheedsust <44333294+waheedsust@users.noreply.github.com>
2020-05-30 23:24:07 +0600

    ex5_drill~.py

---
+++ ex5_drill~.py
@@ -0,0 +1,19 @@
+name = 'Waheed Shams'
+age = 39
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'blue'
+teeth = 'white'
+hair = 'brown'
+height_cm = height * 2.54
+weight_kg = weight * 0.453592
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall")
+print(f"He's {weight} pound heavy")
+print("Actually that's is not too much heavy.")
+print(f"He got {eyes} and {hair} hair.")
+print(f"He's teeth are usually {teeth} depending on coffee.")
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight}, I will get {total}.")
+print(f"{name}'s height is {height_cm} cm")
+print(f"{name}'s weight is {weight_kg} KG")

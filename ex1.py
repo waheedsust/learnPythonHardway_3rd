87c362656ebc502faea20e41b898ef707f3dfe19
waheedsust <44333294+waheedsust@users.noreply.github.com>
2020-05-30 16:37:10 +0600

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#learn python the hard way ex1
+print("Hello World!")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print("Yay! printing this")
+print("I'd much rather you 'not'.")
+print('I "said" do not torch this')
